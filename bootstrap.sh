a0a76e578365cf1ac07c2cd717ad92147d709a5b
Zhang <you@example.com>
2022-01-31 02:42:35 +0000

    modified:   bootstrap.sh 	new file:   install-docker.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,6 @@ curl https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > /roo
     yes | sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
 
 # sudo apt install xfce4 xfce4-goodies
+# sudo apt install tightvncserver
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-20-04
 
