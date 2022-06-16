3b938e829b519e9ad22b2f09e96f0daa507fb7d4
FIRST_NAME <emptyiscolor@gmail.com>
2022-06-15 18:55:58 -0600

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ curl https://raw.githubusercontent.com/wklken/vim-for-server/master/vimrc > /roo
     wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - && \
     echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | \
          sudo tee -a /etc/apt/sources.list.d/virtualbox.list && \
-    sudo apt update && sudo apt install virtualbox-6.1 && \
+    sudo apt update && sudo apt install -y virtualbox-6.1 && \
     wget -c https://download.virtualbox.org/virtualbox/6.1.32/Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack && \
     yes | sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
 
