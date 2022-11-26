861e7e22e6c9bdedb6cf712488ebe14f9d0f60a6
FIRST_NAME <emptyiscolor@gmail.com>
2022-11-26 10:18:27 -0700

    new docker script

--- install-docker.sh
+++ install-docker.sh
@@ -16,9 +16,8 @@ wget -qO- https://get.docker.com/ | sh
 
 # Install docker-compose
 COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 > /usr/local/bin/docker-compose > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
 # move to larger space
 sudo systemctl stop docker &&\
