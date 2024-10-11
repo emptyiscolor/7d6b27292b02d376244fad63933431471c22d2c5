3defa4bbb5bc760f691ac19124c195d8140e2473
yun <emptyiscolor@gmail.com>
2024-10-11 11:10:47 -0600

    fallback

--- profile.py
+++ profile.py
@@ -59,7 +59,6 @@ request = portal.context.makeRequestRSpec()
 
 # Add a raw PC to the request.
 node = request.RawPC("node")
-# node.hardware_type = "m510"
 node.hardware_type = "xl170"
 node.hardware_type = "m400"
 #node.hardware_type = "d430"
